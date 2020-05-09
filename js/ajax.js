2ab162ef12683fc71c5d941f56d5ab023ea97e38
unknown <danrleypow@gmail.com>
2020-05-09 20:39:15 -0300

    Subindo repositórios

---
+++ js/ajax.js
@@ -0,0 +1,15 @@
+let content = document.getElementById('ajax-content')
+
+unction fetchContent(el) {
+    let view = el.getAttribute('a-view')
+    let folder = el.getAttribute('a-folder')
+    fetch('/ajax/${folder}/${view}.html')
+    .then(response => {
+        let html = response.text()
+        
+    })
+    .then(html => { 
+        content.innerHTML = html
+    })
+
+}
